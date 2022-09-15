8390e76b98ad2ca82859a3134ede38160f0e327d
Lukas Gaag <lukas.van.der.gaag@hva.nl>
2022-09-15 13:49:16 +0200

    Assignment 1 setup.

---
+++ Assignment1/src/main/java/models/FreightWagon.java
@@ -0,0 +1,14 @@
+package models;
+// TODO
+public class FreightWagon extends Wagon {
+
+    public FreightWagon(int wagonId, int maxWeight) {
+        super(wagonId);
+        // TODO
+    }
+
+    public int getMaxWeight() {
+        // TODO
+        return 0;
+    }
+}
---
+++ Assignment1/src/main/java/models/PassengerWagon.java
@@ -0,0 +1,15 @@
+package models;
+// TODO
+public class PassengerWagon extends Wagon {
+
+    public PassengerWagon(int wagonId, int numberOfSeats) {
+        super(wagonId);
+        // TODO
+    }
+
+    public int getNumberOfSeats() {
+        // TODO
+
+        return 0;
+    }
+}
