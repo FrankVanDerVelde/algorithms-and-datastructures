29489fd05592d72761518b683496f48be7044697
Frank <frankvdv1998@gmail.com>
2022-11-04 00:26:11 +0100

    Fixed song

---
+++ Assignment3/src/main/java/spotifycharts/Song.java
@@ -0,0 +1,164 @@
+package spotifycharts;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+
+public class Song {
+
+    public enum Language {
+        NL, // Dutch
+        EN, // English
+        DE, // German
+        FR, // French
+        SP, // Spanish
+        IT, // Italian
+    }
+
+    public enum Country {
+        NL, // Netherlands
+        UK, // United Kingdom
+        DE, // Germany
+        BE, // Belgium
+        FR, // France
+        SP, // Spain
+        IT  // Italy
+    }
+
+
+    private final String artist;
+    private final String title;
+    private final Language language;
+
+    // TODO add instance variable(s) to track the streams counts per country
+    //  choose a data structure that you deem to be most appropriate for this application.
+    private final HashMap<Country, Integer> streamsPerCountry;
+
+
+    /**
+     * Constructs a new instance of Song based on given attribute values
+     */
+    public Song(String artist, String title, Language language) {
+        this.artist = artist;
+        this.title = title;
+        this.language = language;
+
+        // TODO initialise streams counts per country as appropriate.
+        this.streamsPerCountry = new HashMap<>();
+    }
+
+    /**
+     * Sets the given streams count for the given country on this song
+     * @param country
+     * @param streamsCount
+     */
+    public void setStreamsCountOfCountry(Country country, int streamsCount) {
+        // TODO register the streams count for the given country.
+
+        streamsPerCountry.put(country, streamsCount);
+    }
+
+    /**
+     * retrieves the streams count of a given country from this song
+     * @param country
+     * @return
+     */
+    public int getStreamsCountOfCountry(Country country) {
+        // TODO retrieve the streams count for the given country.
+
+        return streamsPerCountry.getOrDefault(country, 0);
+    }
+    /**
+     * Calculates/retrieves the total of all streams counts across all countries from this song
+     * @return
+     */
+    public int getStreamsCountTotal() {
+        // TODO calculate/get the total number of streams across all countries
+
+        return streamsPerCountry.values().stream().mapToInt(integer -> integer == null ? 0 : integer).sum();
+    }
+
+
+    /**
+     * compares this song with the other song
+     * ordening songs with the highest total number of streams upfront
+     * @param other     the other song to compare against
+     * @return  negative number, zero or positive number according to Comparator convention
+     */
+    public int compareByHighestStreamsCountTotal(Song other) {
+        // TODO compare the total of stream counts of this song across all countries
+        //  with the total of the other song
+        return Comparator.comparingInt(Song::getStreamsCountTotal).compare(other, this);
+    }
+
+    /**
+     * compares this song with the other song
+     * ordening all Dutch songs upfront and then by decreasing total number of streams
+     * @param other     the other song to compare against
+     * @return  negative number, zero or positive number according to Comparator conventions
+     */
+    public int compareForDutchNationalChart(Song other) {
+        // TODO compare this song with the other song
+        //  ordening all Dutch songs upfront and then by decreasing total number of streams
+
+
+//        assertSame(Element.LI, Element.valueOf("LI"));
+
+//        assertSame(this.getLanguage(), Language.valueOf("NL"));
+
+//        Comparator.comparing((Song s) -> !this.getLanguage().equals( Language.valueOf("NL"))).thenComparing(Integer::getStreamsCountTotal)
+
+
+//       Comparator.comparing(Value::getLanguage)
+
+//        return Comparator.comparing(() -> this.)
+
+//        System.out.println("----------------------");
+//        System.out.println("----------------------");
+//        System.out.println("----------------------");
+//        System.out.println(title);
+//        System.out.println(language);
+//        System.out.println(this.getStreamsCountTotal());
+//        System.out.println("----------------------");
+//        System.out.println(other.title);
+//        System.out.println(other.language);
+//        System.out.println(other.getStreamsCountTotal());
+//        System.out.println("----------------------");
+//        System.out.println("----------------------");
+//        System.out.println("----------------------");
+
+// .thenComparing(Song::getStreamsCountTotal).compare(this, other)
+        System.out.println(title);
+        System.out.println(other.title);
+        System.out.println(Comparator.comparing((Song song) -> song.getLanguage() == Language.NL ? 1 : -1).compare(this, other));
+
+        return Comparator.comparing((Song song) -> song.getLanguage() == Language.NL ? 1 : -1).thenComparing(Song::getStreamsCountTotal).compare(other, this);
+    }
+
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    // TODO provide a toString implementation to format songs as in "artist/title{language}(total streamsCount)"
+
+
+    @Override
+    public String toString() {
+        return new StringJoiner("")
+                .add(artist + "/")
+                .add(title)
+                .add("{" + language + "}")
+                .add("(" + getStreamsCountTotal() + ")")
+                .toString();
+    }
+}
